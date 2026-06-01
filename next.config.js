88656b6b6aff9bac115e8a1559f1c963bf121f3c
Emmanuel Padilla <emmanuelpadilla@Mac-mini-de-Emmanuel.local>
2026-06-01 14:03:08 -0600

    fix: compile in .next on Vercel

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-    // Redirigir el build a un directorio temporal local para evitar bloqueos de sincronización de Google Drive
-    distDir: '/tmp/ave-next-build-master',
+    // Redirigir el build a un directorio temporal local para evitar bloqueos de sincronización de Google Drive, excepto en Vercel.
+    distDir: process.env.VERCEL ? undefined : '/tmp/ave-next-build-master',
 };
 
 export default nextConfig;
